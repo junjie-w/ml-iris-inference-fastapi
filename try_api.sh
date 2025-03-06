29774705a5d397eb062763523a47e728361c4099
Junjie Wu <58950132+junjie-w@users.noreply.github.com>
2025-03-06 15:12:21 +0100

    feat: modify endpoints testing script (#16)

--- try_api.sh
+++ try_api.sh
@@ -3,25 +3,25 @@
 # Make sure the API is running: make run
 
 API_URL="http://127.0.0.1:8000"
-echo "Testing Iris Inference API at $API_URL"
+echo "Trying Iris Inference API at $API_URL"
 
-# Test root endpoint
-echo -e "\n--- Testing root endpoint ---"
+# Root endpoint
+echo -e "\n--- Trying root endpoint ---"
 curl -s $API_URL/
 
-# Test model info endpoint
-echo -e "\n\n--- Testing model info endpoint ---"
+# Model info endpoint
+echo -e "\n\n--- Trying model info endpoint ---"
 curl -s $API_URL/model/info
 
-# Test single prediction
-echo -e "\n\n--- Testing single prediction ---"
+# Single prediction
+echo -e "\n\n--- Trying single prediction ---"
 curl -s -X POST \
   $API_URL/predict \
   -H "Content-Type: application/json" \
   -d '{"sepal_length": 5.1, "sepal_width": 3.5, "petal_length": 1.4, "petal_width": 0.2}'
 
-# Test batch prediction
-echo -e "\n\n--- Testing batch prediction ---"
+# Batch prediction
+echo -e "\n\n--- Trying batch prediction ---"
 curl -s -X POST \
   $API_URL/predict/batch \
   -H "Content-Type: application/json" \
@@ -33,4 +33,4 @@ curl -s -X POST \
     ]
   }'
 
-echo -e "\n\nAPI testing completed"
+echo -e "\n\nAPI works:) 🪻"
